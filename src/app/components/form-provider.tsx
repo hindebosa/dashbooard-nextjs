245970d080ccfb0c1425e48ecf758da9076f764c
EPIC_ELEPHANT\hilto <hindebosa@gmail.com>
2025-03-28 15:01:58 +0200

    textfield-component

--- src/app/components/form-provider.tsx
+++ src/app/components/form-provider.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { UseFormReturn } from 'react-hook-form';
+import type { UseFormReturn } from "react-hook-form";
 
-import { FormProvider as RHFForm } from 'react-hook-form';
+import { FormProvider as RHFForm } from "react-hook-form";
 
 // ----------------------------------------------------------------------
 
